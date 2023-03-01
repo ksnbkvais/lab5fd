19f6e5050a70282f623403da4f19ece8856cec2b
ksnbkvais <114652308+ksnbkvais@users.noreply.github.com>
2023-03-01 21:58:40 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# lab5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-ayhtks)
---
+++ angular-h8sb9g-cfpjrc/src/app/amazon.ts
@@ -0,0 +1,123 @@
+export interface Product {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+    image: string;
+    link: string;
+    rating: number;
+    category: number;
+  }
+  
+  export const products = [
+    {
+      id: 1,
+      name: 'iPhone 13 128Gb',
+      price: 372870,
+      description: 'Ceramic Shield front glass back and aluminum design',
+      image: '../assets/img/1.jpg',
+      rating: 5,
+      link: 'https://kaspi.kz/shop/p/apple-iphone-13-128gb-chernyi-102298404/?c=750000000#!/item',
+      category: 1
+    },
+    {
+      id: 2,
+      name: 'iPhone 14 128Gb',
+      price: 420770,
+      description: 'iOS 16 lets you customize your Lock Screen in fun new ways. Layer a photo to make it pop. Track your Activity rings. And see live updates from your favorite apps.',
+      image: '../assets/img/2.jpg',
+      rating:  5,
+      link: 'https://kaspi.kz/shop/p/apple-iphone-14-128gb-goluboi-106363150/?c=750000000#!/item',
+      category: 1
+    },
+    {
+      id: 3,
+      name: 'iPhone 14 Pro Max 256Gb',
+      price: 695370,
+      description: 'Ceramic Shield front, Textured matte glass back and stainless steel design',
+      image: '../assets/img/3.jpg',
+      rating: 61536,
+      link: 'https://kaspi.kz/shop/p/apple-iphone-14-pro-max-256gb-zolotistyi-106363335/?c=750000000#!/item',
+      category: 1
+    },
+    {
+      id: 4,
+      name: 'iPhone 13 Pro Max 128Gb',
+      price: 662490,
+      description: 'colour:Alpine Green. Ceramic Shield front, Textured matte glass back and stainless steel design',
+      image: '../assets/img/4.jpg',
+      rating:  5,
+      link: 'https://kaspi.kz/shop/p/apple-iphone-13-pro-max-128gb-zelenyi-104079084/?c=750000000#!/item',
+      category: 1
+    },
+  
+    {
+      id: 5,
+      name: 'iPhone 12 128Gb',
+      price: 375050,
+      description: 'A powerful chip. Two-camera system. Incredibly durable Ceramic Shield front panel. And a gorgeous bright OLED display.',
+      image: '../assets/img/5.jpg',
+      rating:  5,
+      link: 'https://kaspi.kz/shop/p/apple-iphone-12-128gb-belyi-100656959/?c=750000000#!/item',
+      category: 1
+    },
+{
+      id: 6,
+      name: 'iPhone 13 128Gb',
+      price: 372870,
+      description: 'Ceramic Shield front glass back and aluminum design',
+      image: '../assets/img/1.jpg',
+      rating: 5,
+      link: 'https://kaspi.kz/shop/p/apple-iphone-13-128gb-chernyi-102298404/?c=750000000#!/item',
+      category: 1
+    },
+    {
+      id: 7,
+      name: 'iPhone 14 128Gb',
+      price: 420770,
+      description: 'iOS 16 lets you customize your Lock Screen in fun new ways. Layer a photo to make it pop. Track your Activity rings. And see live updates from your favorite apps.',
+      image: '../assets/img/2.jpg',
+      rating:  5,
+      link: 'https://kaspi.kz/shop/p/apple-iphone-14-128gb-goluboi-106363150/?c=750000000#!/item',
+      category: 1
+    },
+    {
+      id: 8,
+      name: 'iPhone 14 Pro Max 256Gb',
+      price: 695370,
+      description: 'Ceramic Shield front, Textured matte glass back and stainless steel design',
+      image: '../assets/img/3.jpg',
+      rating: 61536,
+      link: 'https://kaspi.kz/shop/p/apple-iphone-14-pro-max-256gb-zolotistyi-106363335/?c=750000000#!/item',
+      category: 1
+    },
+    {
+      id: 9,
+      name: 'iPhone 13 Pro Max 128Gb',
+      price: 662490,
+      description: 'colour:Alpine Green. Ceramic Shield front, Textured matte glass back and stainless steel design',
+      image: '../assets/img/4.jpg',
+      rating:  5,
+      link: 'https://kaspi.kz/shop/p/apple-iphone-13-pro-max-128gb-zelenyi-104079084/?c=750000000#!/item',
+      category: 1
+    },
+  
+    {
+      id: 10,
+      name: 'iPhone 12 128Gb',
+      price: 375050,
+      description: 'A powerful chip. Two-camera system. Incredibly durable Ceramic Shield front panel. And a gorgeous bright OLED display.',
+      image: '../assets/img/5.jpg',
+      rating:  5,
+      link: 'https://kaspi.kz/shop/p/apple-iphone-12-128gb-belyi-100656959/?c=750000000#!/item',
+      category: 1
+    },
+  
+  ];
+  
+  
+  /*
+  Copyright Google LLC. All Rights Reserved.
+  Use of this source code is governed by an MIT-style license that
+  can be found in the LICENSE file at https://angular.io/license
+  */
